62ca03b4a138ad78f427e208c0e0d4d1297a9205
anks1245 <anks1245@gmail.com>
2022-07-19 17:04:50 +0530

    first commit

---
+++ calc/views.py
@@ -0,0 +1,11 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html',{'name':'Login'})
